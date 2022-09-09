42b58f62b5d0cb53e380418a3e92973325426dff
Alkarias <alkarias29@gmail.com>
2022-09-09 00:12:54 -0400

    refactored to work with v14

--- Index.js
+++ Index.js
@@ -1,6 +1,8 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Message } = require('discord.js');
 const dotenv = require('dotenv');
 dotenv.config();
+
+let botChannel;
 
 const client = new Client({
     intents: [
@@ -12,10 +14,30 @@ const client = new Client({
 
 client.once('ready', () => {
     console.log('Witness Online');
+
+    const channel = '1017643713461239908';
+    botChannel = client.channels.cache.get(channel);
+    const guild = client.guilds.cache.get(process.env.devGuild);
+
+    let commands;
+    if (guild) {
+        commands = guild.commands
+    } else {
+        commands = client.application?.commands
+    }
+
+    commands?.create({
+        name: 'ping',
+        description: 'replies with pong'
+    });
 });
 
 client.on('messageCreate', async (message) => {
-    if (message.content === 'ping') await message.reply('pong');
+    if (message.content === 'ping') {
+        await message.reply('pong');
+        // await botChannel.send(`${'pong'}`);
+    }
 });
+
 
 client.login(process.env.token);
