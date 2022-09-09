ab38b2b36172bcf3d663a21ca6e12ab9a8df0766
Alkarias <alkarias29@gmail.com>
2022-09-08 23:46:11 -0400

    updated discord.js

--- Index.js
+++ Index.js
@@ -1,11 +1,12 @@
-const { Client, Intents } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 const dotenv = require('dotenv');
 dotenv.config();
 
 const client = new Client({
     intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
     ]
 });
 
@@ -13,11 +14,8 @@ client.once('ready', () => {
     console.log('Witness Online');
 });
 
-client.on('message', async (message) => {
-    if (message.content === 'ping') {
-        await message.reply('pong');
-    }
+client.on('messageCreate', async (message) => {
+    if (message.content === 'ping') await message.reply('pong');
 });
-
 
 client.login(process.env.token);
